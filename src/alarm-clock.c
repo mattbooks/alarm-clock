8d301a7cde518d0db3c82d58f44b0dfa6fe3d32d
Matt Books <mattebooks@gmail.com>
2017-01-18 20:03:10 -0800

    Redo loop for adding button polling

--- src/alarm-clock.c
+++ src/alarm-clock.c
@@ -4,8 +4,13 @@
 #include "lib/buffer.h"
 #include "lib/uart.h"
 #include "lib/rtc.h"
+#include "lib/buttons.h"
 
 struct tm* time;
+
+const uint32_t INTERVAL_MS = 10;
+const uint32_t RESET_VAL = 1000000;
+const uint32_t TIME_POLL = 200; // 200 * 10ms = 2s
 
 void init() {
   sei();
@@ -24,10 +29,23 @@ int main()
 
   init_seven_seg();
 
-  while (1) {
-    time = rtc_get_time();
+  uint32_t counter = 0;
 
-    _delay_ms(2000);
+  while (1) {
+    _delay_ms(INTERVAL_MS);
+
+    if (counter % TIME_POLL == 0) {
+      time = rtc_get_time();
+    }
+
+    /* if (counter % BUTTON_POLL == 0) { */
+    /*   // TODO: Poll Buttons */
+    /* } */
+
+    counter += 1;
+    if (counter >= RESET_VAL) {
+      counter = 0;
+    }
   }
 }
 
